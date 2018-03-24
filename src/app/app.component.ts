9133ac84e96266cc368cdc87883226a7626c13c4
jaehongjun <gatsby0391@naver.com>
2018-03-24 18:57:01 +0900

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {RouterLink} from './router-link'
+import { Http,Response,Headers } from '@angular/http'
+import { Observable } from 'rxjs/Observable';
+import { Input } from '@angular/core/src/metadata/directives';
+import { ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  mList : Array<RouterLink> = [] ;
+  url:string = "http://localhost:3000/";
+  loginText: string = "로그인하세요";
+  login:string = "LOGIN";
+  loginVisible:boolean = false; 
+  constructor(protected _http:Http, private elementRef:ElementRef) {
+    // var s = document.createElement("script");
+    // s.type = "text/javascript";
+    // s.src = "/assets/js/jquery.min.js";
+    // this.elementRef.nativeElement.appendChild(s);
+    // var s1 = document.createElement("script");
+    // s1.type = "text/javascript";
+    // s1.src = "/assets/js/main.js";
+    // this.elementRef.nativeElement.appendChild(s1);
+    // var s2 = document.createElement("script");
+    // s2.type = "text/javascript";
+    // s2.src = "/assets/js/skel.min.js";
+    // this.elementRef.nativeElement.appendChild(s2);
+    // var s3 = document.createElement("script");
+    // s3.type = "text/javascript";
+    // s3.src = "/assets/js/util.js";
+    // this.elementRef.nativeElement.appendChild(s3);
+
+    this.menuList().subscribe(
+      res => {
+        console.log(res)
+        this.mList = res.json().list;
+        console.log(this.mList);
+      }
+    )
+  }
+  ngOnInit() {
+
+  }
+  menuList() : Observable<any>{
+     let url : string = "api/menus/"
+     console.log(url);
+    return this._http.get(this.url+url)
+  }
+  childVisible(boolean:Boolean):void{
+    this.loginVisible = false;
+  }
+  buttonView() : void{
+    this.loginVisible = true;
+  }
+  loginStatus(loginMeg:string):void{
+    this.loginText = loginMeg;
+    this.login = "LOGOUT"
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { GenericComponent } from './generic/generic.component';
+import { IndexComponent } from './index/index.component';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
+import { ElemetsComponent } from './elemets/elemets.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component'
+import {LoginService} from './login/login.service';
+import { UserInsertComponent } from './user-insert/user-insert.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenericComponent,
+    IndexComponent,
+    ElemetsComponent,
+    AdminComponent,
+    LoginComponent,
+    UserComponent,
+    UserInsertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,    
+    HttpModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path:'', component:IndexComponent },
+      { path:'elements', component:ElemetsComponent },
+      { path:'generic', component:GenericComponent },
+      { path:'admin', component:AdminComponent },
+      { path:'userInsert', component:UserInsertComponent },
+      { path:'**', component:IndexComponent },
+    ])
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
